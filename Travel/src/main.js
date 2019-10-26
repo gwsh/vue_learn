7fd2c48e9cd493db2e4dd38cf4f9f6c0c3056588
gwsh <shundong106@gmail.com>
2019-10-26 15:00:25 +0800

    VueAwesomeSwiper插件的配置

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-10-25 16:24:33
- * @LastEditTime: 2019-10-26 14:28:27
+ * @LastEditTime: 2019-10-26 14:59:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /Travel/src/main.js
@@ -12,12 +12,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/normalize.css'
 import 'styles/border.css'
 import 'styles/reset.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
