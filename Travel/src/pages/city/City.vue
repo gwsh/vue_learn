c35046ca03c695672bdb41d4b77885027a174a4d
gwsh <shundong106@gmail.com>
2019-11-12 15:42:56 +0800

    city search

--- Travel/src/pages/city/City.vue
+++ Travel/src/pages/city/City.vue
@@ -1,21 +1,27 @@
 <!--
  * @Author: your name
  * @Date: 2019-11-12 10:32:26
- * @LastEditTime: 2019-11-12 14:57:48
+ * @LastEditTime: 2019-11-12 15:26:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /Travel/src/pages/city/City.vue
  -->
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+
 export default {
   name: 'City',
   components: {
-    CityHeader: CityHeader
+    CityHeader: CityHeader,
+    CitySearch: CitySearch
   }
 }
 </script>
---
+++ Travel/src/pages/city/components/Search.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: your name
+ * @Date: 2019-11-12 15:23:41
+ * @LastEditTime: 2019-11-12 15:40:46
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Travel/src/pages/city/components/Search.vue
+ -->
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  .search
+    padding 0 .1rem
+    height .72rem
+    background-color $bgColor
+    .search-input
+      box-sizing border-box
+      width 100%
+      padding 0 .1rem
+      height .62rem
+      line-height  .62rem
+      text-align center
+      border-radius 0.06rem
+</style>
